3bddd4c6f90ba9b68b60796c337e707f31e4fc87
Mikael Moilanen <mikael.moilanen@gmail.com>
2021-11-30 19:07:26 +0200

    Add test for Budget class

---
+++ src/tests/budget_test.py
@@ -0,0 +1,12 @@
+import unittest
+from entities.budget import Budget
+
+class TestBudget(unittest.TestCase):
+    def setUp(self):
+        self.budget = Budget("test_budget", 1, 1)
+        self.budget.income = [(1, "income1", 100), (2, "income2", 200)]
+        self.budget.expenses = [(3, "expense1", 120), (4, "expense2", 150)]
+
+    def test_get_balance(self):
+        balance = self.budget.get_balance()
+        self.assertEqual(balance, 30)
