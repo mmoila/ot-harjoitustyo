0050765599f75919ed791e5937ddd0306364f090
Mikael Moilanen <mikael.moilanen@gmail.com>
2021-12-11 12:37:12 +0200

    Add docstring comments

--- src/entities/budget.py
+++ src/entities/budget.py
@@ -1,5 +1,24 @@
 class Budget:
+    """Luokka, joka kuvastaa äyttäjän yhtä budjettia.
+
+    Attributes: 
+        name = Budjetin nimi.
+        budget_id = Budjetin yksilöivä tunniste.
+        user_id = Käyttäjän yksilöivä tunniste.
+        income = Budjettiin kuuluvat tulot.
+        expenses = Budjettiin kuuluvat menot.
+    """
+
+
     def __init__(self, name, budget_id=None, user_id=None):
+        """Luokan konstruktori, joka luo käyttäjälle uuden budjetin.
+
+        Args:
+            name: Budjetin nimi.
+            budget_id: Budjetin yksilöivä tunniste.. Saa oletuksena arvon None.
+            user_id: Käyttäjän yksilöivä tunniste. Saa oletuksena arvon None.
+        """
+
         self.name = name
         self.user_id = user_id
         self.budget_id = budget_id
@@ -7,6 +26,12 @@ class Budget:
         self.expenses = []
 
     def get_balance(self):
+        """Laskee tulojen ja menojen välisen erotuksen.
+
+        Returns:
+            Palauttaa tulojen ja menojen välisen erotuksen kahden desimaalin tarkkuudella.
+        """
+        
         income_sum = sum([income[2] for income in self.income])
         expenses_sum = sum([expense[2] for expense in self.expenses])
         return round(income_sum - expenses_sum, 2)
--- src/entities/user.py
+++ src/entities/user.py
@@ -1,25 +1,75 @@
 class User:
+    """Luokka, joka kuvastaa yhtä käyttäjää.
+
+    Attributes:
+        id = Käyttäjän yksilöivä id.
+        username = Käyttäjän käyttäjänimi.
+        password = Käyttäjän salasana.
+    """
+
     def __init__(self, username, password, id=None):
+        """Luokan konstruktori, joka luo uuden käyttäjän. Konstruktori
+        hyödyntää luokan asetusmetodeja käyttäjänimen ja salasanan validoinnissa.
+
+        Args:
+            username: Käyttäjän käyttäjänimi.
+            password: Käyttäjän salasana.
+            id: Käyttäjän yksilöivä tunniste. Saa oletuksena arvon None.
+        """
+
         self.id = id
         self.username = username
         self.password = password
 
     @property
     def username(self):
+        """Käyttäjän käyttäjätunnus.
+
+        Returns:
+            Palauttaa käyttäjän käyttäjätunnuksen.
+        """
+
         return self.__username
 
     @property
     def password(self):
+        """Käyttäjän salasana.
+
+        Returns:
+            Palauttaa käyttäjän salasanan.
+        """
+
         return self.__password
 
     @username.setter
     def username(self, username):
+        """Asettaa käyttäjälle käyttäjätunnuksen. Tarkistaa, että annettu
+        salasana vähintään neljä ja enintään 20 merkkiä pitkä.
+
+        Args:
+            username: Käyttäjän käyttäjänimi.
+
+        Raises:
+            ValueError: Virhe, joka tapahtuu jos käyttäjätunnuksen pituus ei
+            ole oikea.
+        """
+
         if len(username) < 4 or len(username) > 20:
             raise ValueError("Username length must be between 4 and 20.")
         self.__username = username
 
     @password.setter
     def password(self, password):
+        """Asettaa käyttäjälle salasanan. Tarkistaa, että annettu salasana 
+        on vähintään kahdeksan ja enintään 25 merkkiä pitkä.
+
+        Args:
+            password: Käyttäjän salasana.
+
+        Raises:
+            ValueError: Virhe, joka tapahtuu jos salasanan pituus ei ole oikea
+        """
+
         if len(password) < 8 or len(password) > 25:
             raise ValueError("Password length must be between 8 and 25.")
         self.__password = password
